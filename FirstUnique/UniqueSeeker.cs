cd238a2cdf00e274dd663a2496b28815de3d1a81
Borya Frolov <bfrolov@renins.com>
2024-07-07 19:57:30 +0300

    the solution

---
+++ FirstUnique/UniqueSeeker.cs
@@ -0,0 +1,37 @@
+﻿namespace FirstUnique;
+
+public static class UniqueSeeker
+{
+    public static int Check(int[] numbers)
+    {
+        var unique = -1;
+
+        for (var i = 0; i < numbers.Length; i += 1)
+        {
+            var isUnique = true;
+            for (var j = 0; j < numbers.Length; j += 1)
+            {
+                // skip self
+                if (i == j)
+                {
+                    continue;
+                }
+
+                // found duplicate
+                if (numbers[i] == numbers[j])
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique)
+            {
+                unique = numbers[i];
+                break;
+            }
+        }
+
+        return unique;
+    }
+}
---
+++ FirstUnique/UniqueSeekerTests.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FirstUnique;
+
+[TestFixture]
+public class UniqueSeekerTests
+{
+    [Test]
+    public void Check_Single()
+    {
+        var result = UniqueSeeker.Check(
+        [
+            4
+        ]);
+
+        result.Should().Be(4);
+    }
+    
+    [Test]
+    public void Check_NoUnique()
+    {
+        var result = UniqueSeeker.Check(
+        [
+            4, 10, 10, 4
+        ]);
+
+        result.Should().Be(-1);
+    }
+    
+    [Test]
+    public void Check_OneUnique()
+    {
+        var result = UniqueSeeker.Check(
+        [
+            4, 10, 5, 4, 2, 10, 2
+        ]);
+
+        result.Should().Be(5);
+    }
+    
+    [Test]
+    public void Check_TwoUnique()
+    {
+        var result = UniqueSeeker.Check(
+        [
+            4, 10, 5, 4, 2, 10
+        ]);
+
+        result.Should().Be(5);
+    }
+    
+    [Test]
+    public void Check_FirstUnique()
+    {
+        var result = UniqueSeeker.Check(
+        [
+            1, 4, 10, 5, 4, 2, 10
+        ]);
+
+        result.Should().Be(1);
+    }
+    
+    [Test]
+    public void Check_LastUnique()
+    {
+        var result = UniqueSeeker.Check(
+        [
+            4, 10, 5, 4, 5, 10, 1
+        ]);
+
+        result.Should().Be(1);
+    }
+    
+    [Test]
+    public void Check_LastNotUnique()
+    {
+        var result = UniqueSeeker.Check(
+        [
+            1, 10, 5, 4, 5, 10, 1
+        ]);
+
+        result.Should().Be(4);
+    }
+}
